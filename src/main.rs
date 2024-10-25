d9471f40c5a3588c41dccfab6abf190199d108c4
JubrilKolade <jubrilkolade97@gmail.com>
2024-10-25 05:00:16 +0100

    day 2 completed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_variables)]
 
-fn main() {
+fn main() {  
     //DAY ONE - variables, mutabilty, shadowing, output, destructuring, datatypes, memory
     println!("Hello World!");
     let x = 5; //immutable
@@ -22,7 +22,7 @@ fn main() {
     
     let slice:&str = &s[0..5];
     
-    // Fix the error below with least amount of modification to the code
+    //1. Fix the error below with least amount of modification to the code
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let _y: i32; // Uninitialized but also unused, only a Warning ! escaped with (_)
 
@@ -97,4 +97,116 @@ fn main() {
     assert_eq!([x,y], [3, 2]); //added[3,2]
 
     println!("Success!");
+
+     //DAY TWO - NUMBERS, SIGNED AND UNSIGNED INTS, FLOATS
+     // 1. Remove something to make it work
+     let x: i32 = 5;
+     let mut y: i32 = 5; //replaced u32 with i32
+ 
+     y = x;
+     
+     let z = 10; // Type of z ? 
+ 
+     println!("Success 1 day2!");
+
+     //2. Fill the blank
+    let v: u16 = 38_u8 as u16;
+
+    println!("Success 2 day 2!");
+
+    
+    // 3. Modify `assert_eq!` to make it work
+
+    let x: u32 = 5;
+    assert_eq!("u32".to_string(), type_of(&x));
+
+    println!("Success 3 day 2!");
+    // Get the type of given variable, return a string representation of the type  , e.g "i8", "u8", "i32", "u32"
+    fn type_of<T>(_: &T) -> String {
+        format!("{}", std::any::type_name::<T>())
+    }
+     
+     
+    //4. Fill the blanks to make it work
+    assert_eq!(i8::MAX, 127); 
+    assert_eq!(u8::MAX, 255); 
+    println!("Success 4 day 2!");
+
+    //5. Fix errors and panics to make it work
+    let v1 = 251_u16 + 8;
+    let v2 = i16::checked_add(251, 8).unwrap();
+    println!("{},{}",v1,v2);
+
+    
+    //6. Modify `assert!` to make it work
+    let v = 1_024 + 0xff + 0o77 + 0b1111_1111;
+    assert!(v == 1597);
+
+    println!("Success 6 day 2!");
+
+
+    //7.  Fill the blank to make it work
+
+    let x: f64 = 1_000.000_1; // ?
+    let y: f32 = 0.12; // f32
+    let z: f64 = 0.01_f64; // f64
+
+    assert_eq!(typeOf(&x), "f64".to_string());
+    println!("Success 7 day 2!");
+    fn typeOf<T>(_: &T) -> String {
+        format!("{}", std::any::type_name::<T>())
+    }
+ 
+    // 8. Make it work in two distinct ways
+    assert!(0.1_f32 + 0.2_f32 == 0.3_f32);
+    println!("Success 8 day 2!");
+
+
+    // 9. Two goals: 1. Modify assert! to make it work 2. Make println! output: 97 - 122
+    let mut sum: i32 = 0;
+    for i in -3..2 {
+        sum += i
+    }
+
+    assert!(sum == -5);
+
+    for c in 'a'..='z' {
+        println!("{}",c as u8);
+    }
+
+    
+    //10. Fill the blanks
+    use std::ops::{Range, RangeInclusive};
+    assert_eq!((1..5), Range{ start: 1, end: 5 });
+    assert_eq!((1..=5), RangeInclusive::new(1, 5));
+
+    println!("Success 10 day 2!");
+
+    
+    //11. Fill the blanks and fix the errors
+
+    // Integer addition
+    assert!(1u32 + 2u32 == 3u32);
+
+    // Integer subtraction
+    assert!(1i32 - 2i32 == -1i32);
+    assert!(1i8 - 2i8 == -1i8); 
+    
+    assert!(3 * 50 == 150);
+
+    assert!(9.6 as f32 / 3.2 as f32 == 3.0 as f32); // error ! make it work
+
+    assert!(24 % 5 == 4);
+    // Short-circuiting boolean logic
+    assert!(true && false == false);
+    assert!(true || false == true);
+    assert!(!true == false);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
 }
